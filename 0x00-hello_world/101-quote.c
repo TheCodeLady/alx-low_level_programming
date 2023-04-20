c12139dbc2a40d025bedb42ad896802b040a178f
Damilola <fadoduno@gmail.com>
2023-04-20 10:59:45 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point to the program
+ *
+ * Return: 1 as argument
+ */
+int main(void)
+{
+	printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return(1)
+}
