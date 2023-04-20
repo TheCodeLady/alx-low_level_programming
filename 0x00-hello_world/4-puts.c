c25f131062546b895d1c39577544689855cfe8f9
Damilola <fadoduno@gmail.com>
2023-04-20 09:07:22 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* This function main will print out a string */
+
+/**
+ * This function main will print out a string 
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
