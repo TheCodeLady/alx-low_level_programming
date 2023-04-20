a73487e15c1df84a5dc888b0b02a7540e33e5086
Damilola <fadoduno@gmail.com>
2023-04-20 09:36:17 -0700

    5-printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 /**
  * main - Prints a string
  *
- * Return - 0 as parameter is void
+ * Return: 0 as parameter is void
  */
 #include <stdio.h>
 int main(void)
---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -this is the entry point
+ *
+ * Return: 0 as parameter is void
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
