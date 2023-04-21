52ea727f592a185fca727f555480a87260c62105
Damilola <fadoduno@gmail.com>
2023-04-21 09:35:36 -0700

    print-numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - point of entry
+ *
+ * Return: 0 as arguement
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		printf("%d \n", number++);
+	}
+
+	return (0);
+}
