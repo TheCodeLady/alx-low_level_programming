5241618db8db4abdea263a8ad2662824a7d896cb
Damilola <fadoduno@gmail.com>
2023-04-21 10:40:56 -0700

    print-comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - point of entry
+ *
+ * Return: 0 to terminate
+ */
+int main(void)
+{
+	int comb = 0;
+
+	while (comb < 10)
+	{
+		putchar(comb + '0');
+		if (comb < 9)
+		{
+			putchar(', ');
+			comb++;
+		}
+	}
+
+	return (0);
+}
