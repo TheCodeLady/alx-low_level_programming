b010da76f8455711f3b86aa01d9caeca4825633d
Damilola <fadoduno@gmail.com>
2023-04-21 11:14:54 -0700

    print-comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - point of entry
+ *
+ * Return: 0 to terminate program
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
