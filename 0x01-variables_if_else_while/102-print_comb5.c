a70fefecb83680f8f3531c3f3080f68139da7aaf
Damilola <fadoduno@gmail.com>
2023-04-21 13:11:05 -0700

    print-comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,21 +16,18 @@ int main(void)
 			{
 				for (num4 = num3; num4 < 10; num4++)
 				{
-					if (num1 && num2 != num3 && num4)
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(' ');
+					putchar(num3 + '0');
+					putchar(num4 + '0');
+					if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
 					{
-						putchar(num1 + '0');
-						putchar(num2 + '0');
-						putchar(' ');
-						putchar(num3 + '0');
-						putchar(num4 + '0');
-						if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
-						{
-							putchar('\n');
-						}
-						else
-						{
-							putchar(',');
-						}
+						putchar('\n');
+					}
+					else
+					{
+						putchar(',');
 					}
 				}
 			}
