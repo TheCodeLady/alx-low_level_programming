59fa3693578af1ff9322f3502655bea22816d4e2
Damilola <fadoduno@gmail.com>
2023-04-21 11:34:37 -0700

    print-comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = num1; num2 < 10; num2++)
+		for (num2 = 1; num2 < 10; num2++)
 		{
 			putchar(num1 + '0');
 			putchar(num2 + '0');
