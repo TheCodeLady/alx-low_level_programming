d883babfa45ed05b537c12aad03a8806c598177c
Damilola <fadoduno@gmail.com>
2023-04-26 07:21:53 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - to get the abs of any input
+ * @int - value type of parameter
+ *
+ * Return: 0 as success
+ */
+int _abs(int)
+{
+	int n;
+	if (n == +n || n == -n || n)
+	{
+		_putchar(n + '0');
+	}
+
+	return (0);
+}
