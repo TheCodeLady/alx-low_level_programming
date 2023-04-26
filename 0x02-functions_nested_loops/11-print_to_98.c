d350e3a5717d05fe828023a49d544e80dd563d17
Damilola <fadoduno@gmail.com>
2023-04-26 10:47:37 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_to_98 - point of entry
+ * @n: integer arguement
+ *
+ * Return: 0 as successful
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d, \n", i);
+	}
+}
