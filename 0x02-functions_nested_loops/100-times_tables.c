b647a8049885be583c0f3aabb0b330e625d2b2dd
Damilola <fadoduno@gmail.com>
2023-04-26 13:12:40 -0700

    times_table

---
+++ 0x02-functions_nested_loops/100-times_tables.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * print_times_table
+ * @n: arguement
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int k;
+
+	if (n > 15 || n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				k = i * j;
+				if (k < 10);
+				{
+					if (j == 0);
+					{
+						_putchar('0');
+					}
+					if (j != 0)
+					{
+						_putchar(' ');
+						_putchar(k + '0');
+					}
+					if (j != 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				else
+				{
+					if (j !=9)
+					{
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
