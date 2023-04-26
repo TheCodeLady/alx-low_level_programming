b186f08b2dd37a48416f64a7a221ccb9fb3fb75c
Damilola <fadoduno@gmail.com>
2023-04-26 04:23:52 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - point of entry
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char aplha = 'a';
+	for (i = alpha; i <= 'z'; i++)
+	{
+		_putchar(aplha);
+	}
+	_putchar('\n');
+
+	return (0);
+}
