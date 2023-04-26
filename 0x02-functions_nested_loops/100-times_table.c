3dcf52a66f6948401cbcd7a2b5e16a5c0ff527df
Damilola <fadoduno@gmail.com>
2023-04-26 14:18:00 -0700

    101-naturals

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,26 +18,22 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				k = i * j;
-				if (k < 10)
+				if (j == 0 && k < 10)
 				{
-					if (j == 0)
-					{
-						_putchar('0');
-					}
-					if (j != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(k + '0');
-					}
-					if (j != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
+					_putchar(k + '0');
+				}
+				if (j != 0 && k < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				if (j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
 				}
 				else
-				{
 					if (j != n)
 					{
 						_putchar(' ');
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - point of entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int sum;
+
+	for (i = 1; i < 1024; i++)
+	{
+		sum = 0;
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+		printf("%d \n", sum);
+	}
+
+	return (0);
+}
