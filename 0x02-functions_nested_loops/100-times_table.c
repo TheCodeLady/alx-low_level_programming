7a656a6d23faef845bec5d0515f88eeae860c091
Damilola <fadoduno@gmail.com>
2023-04-26 13:18:19 -0700

    times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,9 +22,9 @@ void print_times_table(int n)
 			for (j = 0; j < n; j++)
 			{
 				k = i * j;
-				if (k < 10);
+				if (k < 10)
 				{
-					if (j == 0);
+					if (j == 0)
 					{
 						_putchar('0');
 					}
