e2853ac31cd6369971a457dd8f083cc60414ba1c
Damilola <fadoduno@gmail.com>
2023-04-26 08:12:23 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - point of entry
+ * @a: value to get last digit off
+ *
+ * Return: last digit
+ */
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
