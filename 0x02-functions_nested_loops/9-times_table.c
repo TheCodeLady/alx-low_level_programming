b04b18c1dbb9782bd6bfc3478801fa6d1b638fe0
Damilola <fadoduno@gmail.com>
2023-04-26 09:33:11 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - point of entry
+ *
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j != 9)
+			{
+				_putchar((i * j) + '0');
+				_putchar(', ');
+			}
+			else
+			{
+				_putchar((i * j) + '0');
+				_putchar('\n');
+			}
+		}
+	}
+}
