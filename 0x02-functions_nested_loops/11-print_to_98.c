b49a17d9c20085379b57c7fc6f83268127193971
Damilola <fadoduno@gmail.com>
2023-04-26 11:01:31 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,6 +11,18 @@ void print_to_98(int n)
 
 	for (i = n; i <= 98; i++)
 	{
-		printf("%d, \n", i);
+		if (i != 98)
+		{
+			if (n > i)
+			{
+				printf("%d, ", n);
+				n--;
+			}
+			printf("%d, ", i);
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
 	}
 }
