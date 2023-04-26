a457648d9696203e57c9cde87e7116df4335daf5
Damilola <fadoduno@gmail.com>
2023-04-26 14:51:04 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio>
+/**
+ * main - point of entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, fib = 50;
+	long long x = 1, y = 2, z;
+
+	printf("%lld, %lld, ", x, y);
+	for (i = 2; i < n; i++)
+	{
+		z = x + y;
+		printf("%lld, ", z);
+		x = y;
+		y = z;
+	}
+	printf('\n');
+
+	return (0);
+}
