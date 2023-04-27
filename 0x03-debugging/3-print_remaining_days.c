a1e9a59b2a4c7f354a6fccd0c5dff3685d60616e
Damilola <fadoduno@gmail.com>
2023-04-27 12:19:54 -0700

    remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <holberton.h>
 #include "main.h"
 
 /**
@@ -14,7 +15,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 || year % 400 == 0) && !(year % 4 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
