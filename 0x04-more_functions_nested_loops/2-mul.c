fd70ac9c3e0fc20c37357e78494b6ebe8259a11d
Damilola <fadoduno@gmail.com>
2023-04-27 07:35:57 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - entry point
+ * @a: integer variable 1
+ * @b: integer variable 2
+ *
+ * Return: multiplied result of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
