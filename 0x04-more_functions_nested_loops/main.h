57ef974d9679e90d70d2ca4bd0e949eac794f004
Damilola <fadoduno@gmail.com>
2023-04-27 09:00:02 -0700

    corrections

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
 
 int _isupper(int c);
@@ -8,5 +5,4 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-
-#endif /* MAIN_H */
+void more_numbers(void);
