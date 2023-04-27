4b44c3f534114d2168744fa0f02c90df9f2266ed
Damilola <fadoduno@gmail.com>
2023-04-27 08:25:07 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - point of summary
+ *
+ * Return: returns void
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+		}
+		i++;
+	}
+}
+
