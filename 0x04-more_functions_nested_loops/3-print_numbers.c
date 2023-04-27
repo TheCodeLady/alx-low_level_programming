f73a3b476a8096b53d7bf99b27af8356445b8dc5
Damilola <fadoduno@gmail.com>
2023-04-27 07:51:42 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - point of entry
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
