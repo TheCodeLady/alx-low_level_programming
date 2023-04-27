874569323c1bde35b6c2cddf340484dcf3438d04
Damilola <fadoduno@gmail.com>
2023-04-27 07:25:28 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: integer variable
+ *
+ * Return: 0 or 1 depending on the condition
+ */
+int _isdigit(int c)
+{
+
+	while (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
