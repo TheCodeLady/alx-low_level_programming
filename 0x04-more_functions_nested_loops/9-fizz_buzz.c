874a4557156aedee6573e441bd452a4156b4198b
Damilola <fadoduno@gmail.com>
2023-04-27 14:23:55 -0700

    9-fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main - point of entry
+ *
+ * Return: 0 on suceess
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+
+	return (0);
+}
