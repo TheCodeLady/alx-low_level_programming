edafd46eca0c76254078adfb56cca6b1fb2fb22f
Damilola <fadoduno@gmail.com>
2023-05-01 11:43:11 -0700

    corrections

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,21 +24,19 @@ void times_table(void)
 				if (k < 10)
 				{
 					_putchar(' ');
-					_putchar(' ');
 					_putchar(k + '0');
 				}
 				if (k > 9)
 				{
-					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 			}
 			if (j != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
