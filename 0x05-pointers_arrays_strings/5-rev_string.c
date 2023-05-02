e49570632c4f1a0498f18907dc14f3a850a39883
Damilola <fadoduno@gmail.com>
2023-05-02 12:57:55 -0700

    rev-string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,14 @@ void rev_string(char *s)
 	int len = 0;
 	int i;
 
-	while (s[len] != '\0')
+	while (*s != '\0')
 	{
 		len++;
+		s++;
 	}
-	for (i = len - 1; i >= 0; i--)
+	len--;
+	for (i = len; i >= 0; i--)
 	{
-		_putchar(s[i]);
+		_putchar(*s);
 	}
 }
