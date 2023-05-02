574b3c9b1c8a591b0ea338c04cc44e767ca5280b
Damilola <fadoduno@gmail.com>
2023-05-02 13:50:44 -0700

    print-array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,8 +10,16 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = a[0]; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, \n", a[i]);
+		if (i != (n-1))
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
 	}
+	printf("\n");
 }	
