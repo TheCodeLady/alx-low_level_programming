bab99203ed6befc6e6d585522e27aa390e8fafe3
Damilola <fadoduno@gmail.com>
2023-05-02 08:36:14 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - point of entry
+ * @s: string pointer
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (i = length; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
