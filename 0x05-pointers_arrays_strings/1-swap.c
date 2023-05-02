6b83d1589549f75b01b01dc98e1c8f041bfa5d57
Damilola <fadoduno@gmail.com>
2023-05-02 05:11:28 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - point of entry
+ * @a: integer variable
+ * @b: integer variable
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
