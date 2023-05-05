93648d7ef05d5538c02d95a8b79882b823c79d13
Damilola <fadoduno@gmail.com>
2023-05-04 19:02:55 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,5 +9,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	char *dest1 = dest;
 
+	while (*dest1 != '\0')
+	{
+		dest1++;
+	}
+	while (*src < n && *src != '\0')
+	{
+		*dest1 = *src;
+		dest1++;
+		src++;
+	}
+
+	return (dest);
 }
