6fd332c6387f8bbdb9419708a6fd5fbd7c382425
Damilola <fadoduno@gmail.com>
2023-05-04 19:34:02 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * *_strncpy - point of entry
- * @dest: pointer variable 1 
+ * @dest: pointer variable 1
  * @src: pointer variable 2
  * @n: integer variable
  *
@@ -10,6 +10,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
+
 	for (a = 0; a < n && src[a] != '\0'; a++)
 	{
 		dest[a] = src[a];
