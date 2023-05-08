a7b0c14545f5b28240b1cf166fe52eb3411f06ec
Damilola <fadoduno@gmail.com>
2023-05-08 04:59:49 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,16 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
 			return (s);
 		}
-		if (s[i] != c)
-		{
-			return (NULL);
-		}
+		i++;
+	}
+	if (s[i] == '\0')
+	{
+		return ('\0');
 	}
 }
