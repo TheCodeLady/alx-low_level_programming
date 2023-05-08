1a5c2a423da18eacde6e183d3f2df6df8424df0e
Damilola <fadoduno@gmail.com>
2023-05-08 09:15:12 -0700

    8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,11 +14,11 @@ void print_diagsums(int *a, int size)
 	sum2 = 0;
 	for (i = 0; i < (size * size); i++)
 	{
-		if (i % (size + 1))
+		if (i % (size + 1) == 0)
 		{
 			sum1 += a[i];
 		}
-		if (i % (size - 1) && i != 0 && i < ((size * size) - 1))
+		if (i % (size - 1) == 0 && i != 0 && i < ((size * size) - 1))
 		{
 			sum2 += a[i];
 		}
