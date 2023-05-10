c1944896a7158379ae9213048eb60451e0554b9b
Damilola <fadoduno@gmail.com>
2023-05-10 07:45:56 -0700

    prime_num

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,8 +5,23 @@
  *
  * Return: int
  */
+int prime_num(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	if (n % i == 0 && i > 0)
+	{
+		return (0);
+	}
+	return (prime_num(n, i - 1));
+}
 int is_prime_number(int n)
 {
-	if (n > 1)
+	if (n <= 1)
 	{
-		is_prime_number(n 
+		return (0);
+	}
+	return (prime_num(n, n - 1));
+}	
