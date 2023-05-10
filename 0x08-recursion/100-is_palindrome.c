6b4394924bf8f25ce3674d60cc1d0f5dfb79d942
Damilola <fadoduno@gmail.com>
2023-05-10 08:15:54 -0700

    100-101

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * is_prime_number - point of entry
+ * @n: integer variable
+ *
+ * Return: int
+ */
+int is_palindrome(char *s)
+{
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * is_prime_number - point of entry
+ * @n: integer variable
+ *
+ * Return: int
+ */
+int wildcmp(char *s1, char *s2)
+{
+}
