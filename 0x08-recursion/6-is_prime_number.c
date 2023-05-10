27e9d93a364c3cb1fd72596a3473741c960ef9db
Damilola <fadoduno@gmail.com>
2023-05-10 07:48:12 -0700

    prime_num

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * is_prime_number - point of entry
+ * prime_num - point of entry
  * @n: integer variable
+ * @i: integer variable
  *
  * Return: int
  */
@@ -17,6 +18,12 @@ int prime_num(int n, int i)
 	}
 	return (prime_num(n, i - 1));
 }
+/**
+ * is_prime_number - point of entry
+ * @n: integer variable
+ *
+ * Return: int
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)
@@ -24,4 +31,4 @@ int is_prime_number(int n)
 		return (0);
 	}
 	return (prime_num(n, n - 1));
-}	
+}
