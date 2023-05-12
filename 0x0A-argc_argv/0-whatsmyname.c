509460fdb19df190dda8289c977d70f387bc12fb
Damilola <fadoduno@gmail.com>
2023-05-12 11:26:55 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - point of entry
+ * @argc: argument 1 to main
+ * @argv: arg2 to main
+ *
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
