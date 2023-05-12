b0878a55dd1ea9d4728d4b6e62af7d6fb3ad98cb
Damilola <fadoduno@gmail.com>
2023-05-12 13:19:45 -0700

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - point of entry
+ * @argc: argument 1 to main
+ * @argv: arg2 to main
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	if(argc > 0)
+	{
+		for(count = 0; count <= argc; count++)
+		{
+			printf("%s\n", argv[count]);
+		}
+	}
+	else
+	{
+		printf("No arguments supplied");
+	}
+
+	return (0);
+}
