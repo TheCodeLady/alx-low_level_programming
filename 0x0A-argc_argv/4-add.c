97210e0efc0dd6a7695d04b3e039eb7be8d13cfb
Damilola <fadoduno@gmail.com>
2023-05-14 12:31:16 -0700

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <stdlib.h>
+#include <ctype.h>
 /**
  * main - point of entry
  * @argc: argument 1 to main
@@ -7,5 +8,25 @@
  *
  * Return: 0
  */
-int main(int argc, char __attribute__((unused))*argv[])
+int main(int argc, char *argv[])
 {
+	int i = 0, j, k;
+
+	for (j = 1; j < argc; j++)
+	{
+		for (k = 0; argv[j][k]; k++)
+		{
+			if (isdigit(argv[j][k]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (j = 1; j < argc; j++)
+	{
+		i += atoi(argv[j]);
+	}
+	printf("%d\n", i);
+	return (0);
+}
