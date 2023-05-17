da7c855f31233193ba306c65fa95079831a38bc4
Damilola <fadoduno@gmail.com>
2023-05-17 05:16:52 -0700

    2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,9 +9,10 @@
 int main(int argc, char *argv[])
 {
 	int count;
-	if(argc > 0)
+
+	if (argc > 0)
 	{
-		for(count = 0; count < argc; count++)
+		for (count = 0; count < argc; count++)
 		{
 			printf("%s\n", argv[count]);
 		}
