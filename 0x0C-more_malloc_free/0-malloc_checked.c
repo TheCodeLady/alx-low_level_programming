a5fca28e42c4430914e4b67944a4fad58bc3dfa1
Damilola <fadoduno@gmail.com>
2023-05-18 14:36:29 -0700

    0-malloc-checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - point of entry
+ * @b: int variable
+ *
+ * Return: 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+	if (mem == NULL)
+	{
+		return (98);
+	}
+
+	return (mem);
+}
