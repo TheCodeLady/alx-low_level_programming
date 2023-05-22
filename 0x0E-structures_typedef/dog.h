7f8233d283e06409a6b070b7e4962684eaafd2c5
Damilola <fadoduno@gmail.com>
2023-05-22 12:40:30 -0700

    3-dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,8 +15,12 @@ struct dog
 	char *owner;
 };
 
-typedef struct god dog_t;
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif
