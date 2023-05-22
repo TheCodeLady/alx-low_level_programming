463fdbb1cfddc551b5a48de37a03ebe6f00c868e
Damilola <fadoduno@gmail.com>
2023-05-22 07:21:27 -0700

    First commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - object named dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: an object with 3 members
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
