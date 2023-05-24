bb419e948c1c6ab16d3f411a7d70cb150ae83f33
Damilola <fadoduno@gmail.com>
2023-05-24 14:22:43 -0700

    First commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - point of entry
+ * @name: pointer variable
+ * *f - function variable
+ * @char *: parameter
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return;
+	}
+	f(name);
+}
